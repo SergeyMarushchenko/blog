72d6e409d001537b366ea653b5e8c53d89b55e2b
SergeyMarushchenko <marushchenkos@yandex.ru>
2015-10-06 14:29:26 +0300

    Database changed to postgresql

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,8 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+
+  let(:base) {"Blog"}
   
   describe "home page" do
 
@@ -11,7 +13,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
     visit '/static_pages/home'
-    expect(page).to have_title("Blog | Home")
+    expect(page).to have_title("#{base} | Home")
     end
   end
 
@@ -24,7 +26,7 @@ describe "StaticPages" do
 
     it "should have the right title" do
     visit '/static_pages/help'
-    expect(page).to have_title("Blog | Help")
+    expect(page).to have_title("#{base} | Help")
     end
   end
 
@@ -37,7 +39,21 @@ describe "StaticPages" do
 
     it "should have the right title" do
     visit '/static_pages/about'
-    expect(page).to have_title("Blog | About Us")
+    expect(page).to have_title("#{base} | About Us")
+    end
+
+  end
+
+  describe "contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contacts')
+    end
+
+    it "should have the right title" do
+    visit '/static_pages/contact'
+    expect(page).to have_title("#{base} | Contacts")
     end
 
   end
