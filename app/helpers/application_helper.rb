c202ff2f45b93869b46a5f9cfd76fb711b5e886f
SergeyMarushchenko <marushchenkos@yandex.ru>
2015-10-06 19:05:40 +0300

    The full_title helper added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  #returns full page title
+  def full_title(page_title)
+    base_title = "Sergey's Blog"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
+
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -12,9 +12,10 @@ describe "StaticPages" do
     end
 
     it "should have the right title" do
-    visit '/static_pages/home'
-    expect(page).to have_title("#{base} | Home")
+      visit '/static_pages/home'
+      expect(page).to have_title("#{base} | Home")
     end
+
   end
 
   describe "help page" do
@@ -28,6 +29,7 @@ describe "StaticPages" do
     visit '/static_pages/help'
     expect(page).to have_title("#{base} | Help")
     end
+
   end
 
   describe "about page" do
