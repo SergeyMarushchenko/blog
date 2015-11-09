4de5bb48ca8b3bf3cb9d1970ef93c77387bc3b39
SergeyMarushchenko <marushchenkos@yandex.ru>
2015-11-09 13:17:10 +0200

    added last_active_at to users model and view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,12 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  before_action :set_last_seen_at, if: proc { signed_in? && (session[:last_seen_at] == nil || session[:last_seen_at] < 1.minutes.ago) }
+
   protect_from_forgery with: :exception
   include SessionsHelper
+
+  private
+    def set_last_seen_at
+      current_user.update_attribute(:last_seen_at, Time.now)
+      session[:last_seen_at] = Time.now
+    end
 end
